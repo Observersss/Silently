b2dd3af52ec9507ef16e342a58a01d133a8a0d36
Observersss <124256530+Observersss@users.noreply.github.com>
2023-12-04 17:45:47 +0200

    fix bugs

---
+++ Silently/Library/library_BASE_88308.h
@@ -0,0 +1,26 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+#include <QPixmap>
+
+#include <chrono>
+
+#include <ctime>
+
+#include "QMessageBox"
+
+#include <QDateTimeEdit>
+
+#include <QListWidget>
+
+#include <algorithm>
+
+//include <QTimer>
+
+#endif // LIBRARY_H
---
+++ Silently/Library/library_LOCAL_88308.h
@@ -0,0 +1,31 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+#include <QString>
+
+#include <QPixmap>
+
+#include <chrono>
+
+#include <ctime>
+
+#include <QRandomGenerator>
+
+#include "QMessageBox"
+
+#include <QDateTimeEdit>
+
+#include <QListWidget>
+
+#include <algorithm>
+
+#include <map>
+
+//include <QTimer>
+
+#endif // LIBRARY_H
