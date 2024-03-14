d6c0fc996e8a8e93b4bf6e5b7cc41fddbb3574a0
Observersss <124256530+Observersss@users.noreply.github.com>
2024-03-14 15:58:03 +0200

    New functional for update characteristics for character, complete

--- Silently/src/MoreCharacteristics_DialogWindow/morecharacteristics_dialogwindow.cpp
+++ Silently/src/MoreCharacteristics_DialogWindow/morecharacteristics_dialogwindow.cpp
@@ -11,42 +11,59 @@ MoreCharacteristics_DialogWindow::MoreCharacteristics_DialogWindow(QWidget *pare
     updateInfo();
 
     if(show){
-        ui->label_pointCount->setText("12");
+        //встановлюємо базові і мінімальні значення
+        //за принципом -> spinBox_(Необхідна характеристика) setMinimum і setValue character.get(Необхідна характеристика)
         ui->spinBox_health->setMinimum(character->getHealth());
         ui->spinBox_health->setValue(character->getHealth());
-        ui->spinBox_health->setMaximum(character->getHealth()+countPoint);
 
         ui->spinBox_mana->setValue(character->getMana());
-        ui->spinBox_mana->setMaximum(character->getMana());
-        ui->spinBox_mana->setMaximum(character->getMana()+countPoint);
+        ui->spinBox_mana->setMinimum(character->getMana());
 
         ui->spinBox_damage->setValue(character->getDamage());
         ui->spinBox_damage->setMinimum(character->getDamage());
-        ui->spinBox_damage->setMaximum(character->getDamage()+countPoint);
 
         ui->spinBox_chanceOfCriticalDamage->setValue(character->getChanceOfCriticalDamade());
         ui->spinBox_chanceOfCriticalDamage->setMinimum(character->getChanceOfCriticalDamade());
-        ui->spinBox_chanceOfCriticalDamage->setMaximum(character->getChanceOfCriticalDamade()+countPoint);
 
         ui->spinBox_criticalDamage->setValue(character->getCriticalDamage());
         ui->spinBox_criticalDamage->setMinimum(character->getCriticalDamage());
-        ui->spinBox_criticalDamage->setMaximum(character->getCriticalDamage());
 
         ui->spinBox_dexterity->setValue(character->getDexterity());
         ui->spinBox_dexterity->setMinimum(character->getDexterity());
-        ui->spinBox_dexterity->setMaximum(character->getDexterity()+countPoint);
 
         ui->spinBox_force->setValue(character->getForce());
         ui->spinBox_force->setMinimum(character->getForce());
-        ui->spinBox_force->setMaximum(character->getForce()+countPoint);
 
         ui->spinBox_intelligence->setValue(character->getIntelligence());
         ui->spinBox_intelligence->setMinimum(character->getIntelligence());
-        ui->spinBox_intelligence->setMaximum(character->getIntelligence()+countPoint);
 
         ui->spinBox_luck->setValue(character->getLuck());
         ui->spinBox_luck->setMinimum(character->getLuck());
-        ui->spinBox_luck->setMaximum(character->getLuck()+countPoint);
+
+        changeValueEvent();
+
+        boxes[ui->spinBox_health] = character->getHealth();
+        boxes[ui->spinBox_mana] = character->getMana();
+        boxes[ui->spinBox_damage] = character->getDamage();
+        boxes[ui->spinBox_chanceOfCriticalDamage]  = character->getChanceOfCriticalDamade();
+        boxes[ui->spinBox_criticalDamage]  = character->getCriticalDamage();
+        boxes[ui->spinBox_dexterity] = character->getDexterity();
+        boxes[ui->spinBox_force] = character->getForce();
+        boxes[ui->spinBox_intelligence] = character->getIntelligence();
+        boxes[ui->spinBox_luck] = character->getLuck();
+
+
+        //connect всіх spinBox_() при події valueChanged для виклику on_spinBox_valueChanged
+        connect(ui->spinBox_health, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_mana, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_damage, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_chanceOfCriticalDamage, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_criticalDamage, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_dexterity, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_force, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_intelligence, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+        connect(ui->spinBox_luck, SIGNAL(valueChanged(int)), this, SLOT(on_spinBox_valueChanged(int)));
+
     }else{
         int newWidth = width() / 2;
         resize(newWidth, height());
@@ -71,7 +88,8 @@ void MoreCharacteristics_DialogWindow::on_pushButton_clicked()
     character->setIntelligence(ui->spinBox_intelligence->value());
     character->setLuck(ui->spinBox_luck->value());
 }
-void  MoreCharacteristics_DialogWindow::updateInfo(){
+void MoreCharacteristics_DialogWindow::updateInfo(){
+    //Виивід характеристик персонажа у необхідні поля
     QString healthText = QString("%1").arg(character->getHealth());
     ui->HealthLabel->setText(healthText);
 
@@ -85,7 +103,7 @@ void  MoreCharacteristics_DialogWindow::updateInfo(){
     ui->ExperienceLabel->setText(experinceText);
 
     QString damageText = QString("%1").arg(character->getDamage());
-    ui->DamgeLabel->setText(damageText);
+    ui->DamageLabel->setText(damageText);
 
     QString chanceOfCriticalDamadeText = QString("%1").arg(character->getChanceOfCriticalDamade());
     ui->ChanceLabel->setText(chanceOfCriticalDamadeText);
@@ -104,5 +122,31 @@ void  MoreCharacteristics_DialogWindow::updateInfo(){
 
     QString luckText = QString("%1").arg(character->getLuck());
     ui->LucklLabel->setText(luckText);
+}
+void MoreCharacteristics_DialogWindow::changeValueEvent(){
+    //Встановлюємо доступну кількість вільних очок і оновлюємо максимальні значення на основі теперешнього+кількість вільних очок
+    ui->label_pointCount->setText(QString::number(countPoint));
+
+    ui->spinBox_health->setMaximum(ui->spinBox_health->value()+countPoint);
+    ui->spinBox_mana->setMaximum(ui->spinBox_mana->value()+countPoint);
+    ui->spinBox_damage->setMaximum(ui->spinBox_damage->value()+countPoint);
+    ui->spinBox_chanceOfCriticalDamage->setMaximum(ui->spinBox_chanceOfCriticalDamage->value()+countPoint);
+    ui->spinBox_criticalDamage->setMaximum(ui->spinBox_criticalDamage->value()+countPoint);
+    ui->spinBox_dexterity->setMaximum(ui->spinBox_dexterity->value()+countPoint);
+    ui->spinBox_force->setMaximum(ui->spinBox_force->value()+countPoint);
+    ui->spinBox_intelligence->setMaximum(ui->spinBox_intelligence->value()+countPoint);
+    ui->spinBox_luck->setMaximum(ui->spinBox_luck->value()+countPoint);
+}
+
+void MoreCharacteristics_DialogWindow::on_spinBox_valueChanged(int arg1)
+{
+    QSpinBox *senderSpinBox = qobject_cast<QSpinBox*>(sender());
+    if (!senderSpinBox)
+        return;
+
+    int value = boxes[senderSpinBox] - arg1;
+    value > 0 ? countPoint++ : countPoint--;
+    boxes[senderSpinBox] = arg1;
+    changeValueEvent();
 }
 
--- Silently/src/MoreCharacteristics_DialogWindow/morecharacteristics_dialogwindow.h
+++ Silently/src/MoreCharacteristics_DialogWindow/morecharacteristics_dialogwindow.h
@@ -3,6 +3,8 @@
 
 #include <QDialog>
 #include "RPGspace/Character/character.h"
+#include <map>
+#include <QSpinBox>
 namespace Ui {
 class MoreCharacteristics_DialogWindow;
 }
@@ -16,14 +18,35 @@ public:
     ~MoreCharacteristics_DialogWindow();
 
     void updateInfo();
-
+    void changeValueEvent();
+public slots:
+    void on_spinBox_valueChanged(int arg1);
 private slots:
     void on_pushButton_clicked();
+
+    // void on_spinBox_health_valueChanged(int arg1);
+
+    // void on_spinBox_mana_valueChanged(int arg1);
+
+    // void on_spinBox_damage_valueChanged(int arg1);
+
+    // void on_spinBox_chanceOfCriticalDamage_valueChanged(int arg1);
+
+    // void on_spinBox_criticalDamage_valueChanged(int arg1);
+
+    // void on_spinBox_dexterity_valueChanged(int arg1);
+
+    // void on_spinBox_force_valueChanged(int arg1);
+
+    // void on_spinBox_intelligence_valueChanged(int arg1);
+
+    // void on_spinBox_luck_valueChanged(int arg1);
 
 private:
     Ui::MoreCharacteristics_DialogWindow *ui;
     Character* character;
     int countPoint = 12;
+    std::map<QSpinBox*,int> boxes;
 };
 
 #endif // MORECHARACTERISTICS_DIALOGWINDOW_H
