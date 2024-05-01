fe677a44b3d860f469e0cfece24985df74e741a0
VikaBilyk <128689260+VikaBilyk@users.noreply.github.com>
2024-05-01 03:18:02 +0300

    Test 1

--- Silently/src/NOTEspace/NoteService/noteservice.cpp
+++ Silently/src/NOTEspace/NoteService/noteservice.cpp
@@ -1,222 +1,366 @@
 #include "noteservice.h"
 
-std::vector<Tag> NoteService::allTag;
+QVector<Tag> NoteService::allTag_;
 
-NoteService::NoteService()
-{
-    Note note;
-    nameSpaceNote="FirstNoteSpace";
-    note.setTitle("Your first note");
-    notes.push_back(note);
-    NameNoteAndNoteID.push_back(std::pair(note.getTitle(),note.getIdNote()));
-}
-NoteService::NoteService(QString nameSpace):nameSpaceNote(nameSpace){
-    Note note;
-    note.setTitle("Your first note");
-    notes.push_back(note);
-}
-void NoteService::addNote(Note& note){
-    notes.push_back(note);
-}
-void NoteService::deleteNote(QString &noteTitle){
-    //Використовуюємо цикл foreach для перебору елементів вектора
-    for (auto it = notes.begin(); it != notes.end(); ++it) {
+// NoteService::NoteService()
+// {
+//     Note note;
+//     nameSpaceNote="FirstNoteSpace";
+//     note.setTitle("Your first note");
+//     notes.push_back(note);
+//     NameNoteAndNoteID.push_back(std::pair(note.getTitle(),note.getIdNote()));
+// }
+// NoteService::NoteService(QString nameSpace):nameSpaceNote(nameSpace){
+//     Note note;
+//     note.setTitle("Your first note");
+//     notes.push_back(note);
+// }
+// void NoteService::addNote(Note& note){
+//     notes.push_back(note);
+// }
+// void NoteService::deleteNote(QString &noteTitle){
+//     for (auto it = notes.begin(); it != notes.end(); ++it) {
+//         if (it->getTitle() == noteTitle) {
+//             notes.erase(it);
+//             break;
+//         }
+//     }
+// }
+// Note NoteService::getNote(QString &noteTitle){
+//     for (const Note& note : notes) {
+//         if (note.getTitle() == noteTitle) {
+//             return note;
+//         }
+//     }
 
-        //Порівнюємо назву квеста з getTitile()
-        if (it->getTitle() == noteTitle) {
+//     Note note_error;
+//     note_error.setTitle(nullptr);
+//     return note_error;
+// }
+// Note NoteService::getNote(int &id){
+//     for (const Note& note : notes) {
+//         if (note.getIdNote() == id) {
+//             return note;
+//         }
+//     }
 
-            //Видаляємо квест з вектора
-            notes.erase(it);
-            break; //Ми знайшли і видалили квест тому виходимо з цикла
-        }
+//     Note note_error;
+//     note_error.setTitle(nullptr);
+//     return note_error;
+// }
+
+// void NoteService::setChangNote(Note note){
+//     for (Note &existingNote : notes) {
+//         if (existingNote.getDataTime() == note.getDataTime()) {
+//             existingNote = note;
+//             return;
+//         }
+//     }
+// }
+
+// void NoteService::setChangNote(Note note,QString buffer){
+//     for (Note &existingNote : notes) {
+//         if (existingNote.getTitle() == buffer) {
+//             existingNote = note;
+//             return;
+//         }
+//     }
+// }
+
+// Note* NoteService::getFirstNote(){
+//     return &notes[0];
+// }
+
+// bool NoteService::noteExists(QDateTime  noteTime) {
+//     for (const Note& note : notes) {
+//         if (note.getDataTime() == noteTime) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+// bool NoteService::noteExists(QString noteTitle){
+//     for (const Note& note : notes) {
+//         if (note.getTitle() == noteTitle) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+// bool NoteService::noteExists(int noteID){
+//     for (const Note& note : notes) {
+//         if (note.getIdNote() == noteID) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+// void NoteService::tagsExists(const std::vector<QString> &tagNames) {
+//     bool found = false;
+
+//     for (const QString &tagName : tagNames) {
+//         for (const Tag &tag : allTag) {
+//             if (tag.getNameTag() == tagName) {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//         if (found) {
+//             found = false;
+//         } else {
+//             addToAllTag(tagName);
+//         }
+//     }
+// }
+
+// void NoteService::setNameSpaceNote(QString name){
+//     nameSpaceNote=name;
+// }
+// void NoteService::addToAllTag(QString tagName){
+//     Tag newTag(tagName);
+//     allTag.push_back(newTag);
+// }
+// std::vector<Note> NoteService::getAllNotes()const{
+//     return notes;
+// }
+// std::vector<Note>* NoteService::getAllNotesPtr(){
+//     return &notes;
+// }
+
+// QString NoteService::getNameSpaceNote() const{
+//     return nameSpaceNote;
+// }
+
+// Note* NoteService::getNotePtr(const int &id) {
+//     for (Note& note : notes) {
+//         if (note.getIdNote() == id) {
+//             return &note;
+//         }
+//     }
+
+//     return nullptr;
+// }
+// Note* NoteService::getNotePtr(const QString &title){
+//     for(Note& note : notes){
+//         if(note.getTitle() == title){
+//             return &note;
+//         }
+//     }
+
+//     return nullptr;
+// }
+// Note* NoteService::getNotePtr(const QDateTime& data_create){
+//     for(Note& note : notes){
+//         if(note.getDataTime() == data_create){
+//             return &note;
+//         }
+//     }
+
+//     return nullptr;
+// }
+
+// int NoteService::findIdNote(QString nameNote){
+//     for (const auto& pair : NameNoteAndNoteID) {
+//         if (pair.first == nameNote) {
+//             return pair.second;
+//         }
+//     }
+
+//     return -1;
+// }
+// void NoteService::changeNameNoteInVector(QString newName,int oldID){
+//     for (auto& pair : NameNoteAndNoteID) {
+//         if (pair.second == oldID) {
+//             pair.first = newName;
+//             return;
+//         }
+//     }
+// }
+
+// void NoteService::removeNoteFromVector(int oldID){
+//     auto it = std::remove_if(NameNoteAndNoteID.begin(), NameNoteAndNoteID.end(),
+//                              [oldID](const auto& pair) { return pair.second == oldID; });
+
+//     if (it != NameNoteAndNoteID.end()) {
+//         NameNoteAndNoteID.erase(it, NameNoteAndNoteID.end());
+//     }
+// }
+// void NoteService::addNewElementToNameNoteAndNoteID(QString title,int id){
+//     NameNoteAndNoteID.push_back(std::pair(title,id));
+// }
+// std::vector<std::pair<QString,int>> NoteService::returnNameNoteAndNoteID()const{
+//     return NameNoteAndNoteID;
+// }
+
+NoteService::NoteService(){
+    Note* note  = NoteFactory::create();
+    nameSpaceNote_ = "FirstNoteSpace";
+    note->setTitle(BASIC_NAME_NOTE);
+    addNote(note);
+    addNewElement_to_NameNoteAndNoteID(note->getTitle(),note->getIdNote());
+}
+NoteService::NoteService(const QString& nameSpace){
+    nameSpaceNote_ = nameSpace;
+    Note* note = NoteFactory::create();
+    note->setTitle(BASIC_NAME_NOTE);
+    addNote(note);
+    addNewElement_to_NameNoteAndNoteID(note->getTitle(),note->getIdNote());
+}
+void NoteService::setNameSpaceNote(const QString &name){
+    nameSpaceNote_ = name;
+}
+
+void NoteService::addNote(Note* note){
+    if(!note->getTitle().isEmpty()){
+        notes_.push_back(note);
+        addNewElement_to_NameNoteAndNoteID(note->getTitle(),note->getIdNote());
+    }else{
+        throw std::runtime_error("Fail! Title is empty /NoteService::addNote(Note* note)");
+        return;
     }
 }
-Note NoteService::getNote(QString &noteTitle){
-    for (const Note& note : notes) {
-        if (note.getTitle() == noteTitle) {
-            return note; // Якщо квест знайшли то виходимо з функції
-        }
-    }
 
-    //Квест не знайдемо виведеться помилка при перевірці чи повернуло квест
-    Note note_error;
-    note_error.setTitle(nullptr);
-    return note_error;
-}
-Note NoteService::getNote(int &id){
-    for (const Note& note : notes) {
-        if (note.getIdNote() == id) {
-            return note; // Якщо квест знайшли то виходимо з функції
-        }
-    }
-
-    //Квест не знайдемо виведеться помилка при перевірці чи повернуло квест
-    Note note_error;
-    note_error.setTitle(nullptr);
-    return note_error;
-}
-
-void NoteService::setChangNote(Note note){
-    for (Note &existingNote : notes) {
-        if (existingNote.getDataTime() == note.getDataTime()) {
-            existingNote = note; // Заменяем существующий объект новым объектом
-            return; // Можно выйти из цикла, так как объект найден и заменен
-        }
-    }
-}
-
-void NoteService::setChangNote(Note note,QString buffer){
-    for (Note &existingNote : notes) {
-        if (existingNote.getTitle() == buffer) {
-            existingNote = note; // Заменяем существующий объект новым объектом
-            return; // Можно выйти из цикла, так как объект найден и заменен
-        }
-    }
-}
-//void NoteService::setChangNote(int& id,Note &note){
-//    for (Note &existingNote : notes) {
-//        if (existingNote.getIdNote()==id) {
-//            existingNote = note; // Заменяем существующий объект новым объектом
-//            return; // Можно выйти из цикла, так как объект найден и заменен
-//        }
-//    }
-//}
-
-Note* NoteService::getFirstNote(){
-    return &notes[0];
-}
-
-bool NoteService::noteExists(QDateTime  noteTime) {
-    for (const Note& note : notes) {
-        // Предполагаем, что getTime() возвращает время создания заметки типа QDateTime
-        if (note.getDataTime() == noteTime) {
-            return true; // Если найдена заметка с указанным временем, возвращаем true
-        }
-    }
-    return false; // Если заметка с указанным временем не найдена, возвращаем false
-}
-bool NoteService::noteExists(QString noteTitle){
-    for (const Note& note : notes) {
-        if (note.getTitle() == noteTitle) {
-            //qDebug()<<"Название заметки правильной заметки"<<note.getTitle();
-            //qDebug()<<"Название заметки"<<noteTitle;
-            return true; // Нотатка з таким заголовком існує повертаємо true
-        }
-        //qDebug()<<"Остальные заметки\n";
-        //qDebug()<<note.getTitle();
-        //qDebug()<<"Название заметки"<<noteTitle;
-    }
-    return false;// Нотатка не існує тому false
-}
-bool NoteService::noteExists(int noteID){
-    for (const Note& note : notes) {
-        // Предполагаем, что getTime() возвращает время создания заметки типа QDateTime
-        if (note.getIdNote() == noteID) {
-            //qDebug()<<"ID какое нашло при проверки noteExists"<<note.getIdNote();
-            return true; // Если найдена заметка с указанным временем, возвращаем true
-        }
-    }
-    //qDebug()<<"ID какое не найдено при проверки noteExists"<<noteID;
-    return false; // Если заметка с указанным временем не найдена, возвращаем false
-}
-
-void NoteService::tagsExists(const std::vector<QString> &tagNames) {
-    bool found = false;
-
-    for (const QString &tagName : tagNames) {
-        for (const Tag &tag : allTag) {
-            if (tag.getNameTag() == tagName) {
-                found = true;
-                // Действия при совпадении тегов (можно добавить свой код)
-                break;
-            }
-        }
-        if (found) {
-            // Если тег найден, можно добавить нужные действия здесь
-            found = false; // Сброс флага
+void NoteService::deleteNote(const QString& noteTitle) {
+    Note* find = findNote(noteTitle);
+    if (find != nullptr) {
+        removeElement_from_nameNoteAndNoteID(find->getIdNote());
+        delete find;
+        int index = notes_.indexOf(find);
+        if (index != -1) {
+            notes_.remove(index);
         } else {
-            addToAllTag(tagName);
+            throw std::runtime_error("Failed to find note 1 /NoteService::deleteNote");
         }
+    } else {
+        throw std::runtime_error("Failed to find note 1.1 /NoteService::deleteNote");
     }
 }
 
-void NoteService::setNameSpaceNote(QString name){
-    nameSpaceNote=name;
-}
-void NoteService::addToAllTag(QString tagName){
-    Tag newTag(tagName);
-    allTag.push_back(newTag);
-}
-std::vector<Note> NoteService::getAllNotes()const{
-    return notes;
-}
-std::vector<Note>* NoteService::getAllNotesPtr(){
-    return &notes;
+void NoteService::addToAllTag(const QString& tagName){
+    allTag_.push_back(Tag::create(tagName));
 }
 
-QString NoteService::getNameSpaceNote() const{
-    return nameSpaceNote;
+void NoteService::setChangeNote(Note *note){
+    Note* find = findNote(note->getTitle());
+
+    if(find != nullptr){
+        *find = *note;
+        changeInfo_into_NameNoteInVector(find->getTitle(),find->getIdNote());
+        delete note;
+    }else{
+        throw std::runtime_error("Faild change note, Note* find == nullptr /NoteService::setChangeNote(Note *note)");
+    }
 }
 
-Note* NoteService::getNotePtr(const int &id) {
-    for (Note& note : notes) {
-        if (note.getIdNote() == id) {
-            return &note; // Возвращаем указатель на объект Note из списка notes
-        }
+void NoteService::setChangeNote(Note* note, const QString& oldName) {
+    Note* find = findNote(oldName);
+
+    if(find != nullptr){
+        *find = *note;
+        changeInfo_into_NameNoteInVector(find->getTitle(),find->getIdNote());
+        delete note;
+    }else{
+        throw std::runtime_error("Faild change note, Note* find == nullptr /NoteService::setChangeNote(Note* note, const QString& oldName)");
     }
-    return nullptr; // Если заметка не найдена, возвращаем nullptr
 }
-Note* NoteService::getNotePtr(const QString &title){
-    for(Note& note : notes){
-        if(note.getTitle() == title){
-            return &note;
-        }
+
+int NoteService::findIdNote(const QString& nameNote){
+    return nameNoteAndNoteID_[nameNote];
+}
+
+Note* NoteService::getFirstNote() const{
+    if(!notes_.isEmpty()){
+        return notes_[0];
+    }else{
+        throw std::runtime_error("Fail! notes_ is empty /NoteService::getFirstNote()");
+        return nullptr;
     }
+}
+
+Note* NoteService::getNote(const int &id){
+    Note* find = findNote(id);
+    if(find != nullptr){
+        return find;
+    }else{
+        throw std::runtime_error("Fail! find is nullptr /NoteService::getNote(const int &id)");
+        return nullptr;
+    }
+}
+Note* NoteService::getNote(const QString& name){
+    Note* find = findNote(name);
+    if(find != nullptr){
+        return find;
+    }else{
+        throw std::runtime_error("Fail! find is nullptr /NoteService::getNote(const QString& name)");
+        return nullptr;
+    }
+}
+Note* NoteService::getNote(const QDateTime& dateOfCreation){
+    Note* find = findNote(dateOfCreation);
+    if(find != nullptr){
+        return find;
+    }else{
+        throw std::runtime_error("Fail! find is nullptr /NoteService::getNote(const QDateTime& dateOfCreation)");
+        return nullptr;
+    }
+}
+template <typename T>
+Note* NoteService::getNote(const T& arg) {
+    static_assert(std::is_same<T, int>::value || std::is_same<T, QString>::value || std::is_same<T, QDateTime>::value,
+                  "Function getNote can only be called with int, QString, or QDateTime");
     return nullptr;
 }
-Note* NoteService::getNotePtr(const QDateTime& data_create){
-    for(Note& note : notes){
-        if(note.getDataTime() == data_create){
-            return &note;
+QVector<Note*> NoteService::getAllNotes()const{
+    return notes_;
+}
+QString NoteService::getNameSpaceNote()const{
+    return nameSpaceNote_;
+}
+std::unordered_map<QString,int> NoteService::returnNameNoteAndNoteID()const{
+    return nameNoteAndNoteID_;
+}
+template<typename T>
+Note* NoteService::findNote(const T& value){
+    auto it = std::find_if(notes_.begin(), notes_.end(), [&value](const Note* note) {
+        return *note == value;
+    });
+
+    if(it != notes_.end()){
+        return *it;
+    } else {
+        throw std::runtime_error("Failed to find note /NoteService::findNote");
+        return nullptr;
+    }
+}
+
+void NoteService::addNewElement_to_NameNoteAndNoteID(const QString& title,const int& id){
+    nameNoteAndNoteID_[title] = id;
+}
+void NoteService::changeInfo_into_NameNoteInVector(const QString& newName, const int& oldID) {
+    auto it = std::find_if(nameNoteAndNoteID_.begin(), nameNoteAndNoteID_.end(),
+                           [oldID](const auto& pair) { return pair.second == oldID; });
+
+    if (it != nameNoteAndNoteID_.end()) {
+        int value = it->second;
+        nameNoteAndNoteID_.erase(it);
+        nameNoteAndNoteID_[newName] = value;
+    } else {
+        //if element not founded
+        throw std::runtime_error("Fail! cannot find element /NoteService::changeInfo_into_NameNoteInVector(const QString& newName,const int& oldID)");
+    }
+}
+
+void NoteService::removeElement_from_nameNoteAndNoteID(const int& id){
+    for (auto it = nameNoteAndNoteID_.begin(); it != nameNoteAndNoteID_.end(); ++it) {
+        if (it->second == id) {
+            nameNoteAndNoteID_.erase(it);
+            return;
         }
     }
-    return nullptr;
+    //if element not founded
+    throw std::runtime_error("Fail! cannot find element /NoteService::removeElement_from_nameNoteAndNoteID(const int& id)");
 }
-
-int NoteService::findIdNote(QString nameNote){
-    for (const auto& pair : NameNoteAndNoteID) {
-        if (pair.first == nameNote) {
-//            qDebug()<<"Название заметки при поиске findIdNote"<<pair.first;
-//            qDebug()<<"ID какое возвращает функция поиска findIdNote"<<pair.second;
-            return pair.second; // Возвращаем идентификатор заметки, если найдено соответствие
-        }
-        //qDebug()<<"Название заметки при поиске findIdNote"<<pair.first;
-    }
-    return -1; // Возвращаем -1, если заметка не найдена
-}
-void NoteService::changeNameNoteInVector(QString newName,int oldID){
-    for (auto& pair : NameNoteAndNoteID) {
-        if (pair.second == oldID) {
-            pair.first = newName;
-            //qDebug()<<"Сохранение прошло успешно";
-            return; // Если нашли идентификатор, меняем значение и выходим из цикла
-        }
-    }
-    //qDebug()<<"Сохранение не удалось";
-}
-
-void NoteService::removeNoteFromVector(int oldID){
-    auto it = std::remove_if(NameNoteAndNoteID.begin(), NameNoteAndNoteID.end(),
-                             [oldID](const auto& pair) { return pair.second == oldID; });
-
-    if (it != NameNoteAndNoteID.end()) {
-        NameNoteAndNoteID.erase(it, NameNoteAndNoteID.end());
-    }
-}
-void NoteService::addNewElementToNameNoteAndNoteID(QString title,int id){
-    NameNoteAndNoteID.push_back(std::pair(title,id));
-}
-std::vector<std::pair<QString,int>> NoteService::returnNameNoteAndNoteID()const{
-    return NameNoteAndNoteID;
-}
-
