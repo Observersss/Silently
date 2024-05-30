e4578777ae6629063b5eb35016a0415c719065f9
VikaBilyk <124256530+Observersss@users.noreply.github.com>
2024-05-30 20:41:22 +0300

    repair RPGspace_test complete

--- Silently/test/RPGSpace_Test/tst_rpgspace_test.cpp
+++ Silently/test/RPGSpace_Test/tst_rpgspace_test.cpp
@@ -43,127 +43,119 @@ void RPGSpace_test::cleanupTestCase() {}
 
 // Test adding a quest to active quests
 void RPGSpace_test::testAddActiveQuest() {
-    // Character character;
-    // Quest* quest = QuestFactory::create();
-    // character.addActiveQuest(quest);
-    // QCOMPARE(character.getActiveQuest().size(), size_t(1));
+    Character character;
+    std::shared_ptr<Quest> quest = QuestFactory::create();
+    character.addActiveQuest(quest);
+    QCOMPARE(character.getActiveQuest().size(), size_t(1));
 }
 
 // Test deleting a quest from active quests
 void RPGSpace_test::testDeleteActiveQuest() {
-    // Character character;
+    Character character;
 
-    // Quest* quest = QuestFactory::create();
-    // character.addActiveQuest(quest);
-    // character.deleteActiveQuest(quest);
+    std::shared_ptr<Quest> quest = QuestFactory::create();
+    character.addActiveQuest(quest);
+    character.deleteActiveQuest(quest);
 
-    // QCOMPARE(character.getActiveQuest().size(), size_t(0));
+    QCOMPARE(character.getActiveQuest().size(), size_t(0));
 }
 
 
 
 void RPGSpace_test::testAddItemToInventory() {
-    // Inventory inventory;
-    // Item item;
-    // inventory.addItemInInventory(item);
+    Inventory inventory;
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    inventory.addItem(item);
 
-    // QCOMPARE(inventory.getItemInInventoryCount(), size_t(1));
+    QCOMPARE(inventory.getSize(), size_t(1));
 }
 
 // Тестирование удаления предмета из инвентаря
 void RPGSpace_test::testRemoveItemFromInventory() {
-    // Inventory inventory;
-    // Item item;
-    // inventory.addItemInInventory(item);
-    // inventory.deleteItemInInventory(item);
+    Inventory inventory;
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    inventory.addItem(item);
+    inventory.removeItem(item);
 
-    // QCOMPARE(inventory.getItemInInventoryCount(), size_t(0));
+    QCOMPARE(inventory.getSize(), size_t(0));
 }
 
 // Тестирование добавления предмета в снаряжение
 void RPGSpace_test::testAddItemToEquipment() {
-    // Inventory inventory;
-    // Item item;
-    // inventory.addToEquipment(item);
+    Character character;
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    character.addItemToInventory(item);
+    character.addItemToEquipment(item);
 
-    // QCOMPARE(inventory.getItemInEquipCount(), size_t(1));
+    QCOMPARE(character.getEquipment()->getSize(), size_t(1));
 }
 
 // Тестирование удаления предмета из снаряжения
 void RPGSpace_test::testRemoveItemFromEquipment() {
-    // Inventory inventory;
-    // Item item;
-    // inventory.addToEquipment(item);
-    // inventory.removeFromEquipment(item);
+    Character character;
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    character.addItemToInventory(item);
+    character.addItemToEquipment(item);
+    character.removeItemFromEquipment(item);
 
-    // QCOMPARE(inventory.getItemInEquipCount(), size_t(0));
+    QCOMPARE(character.getEquipment()->getSize(),size_t(0));
 }
 
 void RPGSpace_test::testRankGeneration() {
-    // Item item;
-    // QVERIFY(item.getRank() == "S" || item.getRank() == "A" || item.getRank() == "B" ||
-    //         item.getRank() == "C" || item.getRank() == "D");
-
-    // Inventory inventory;
-    // Item item;
-    // inventory.addToEquipment(item);
-    // inventory.removeFromEquipment(item);
-
-    // QCOMPARE(inventory.getItemInEquipCount(), size_t(0));
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    QVERIFY(item->getRank() == "S" || item->getRank() == "A" || item->getRank() == "B" ||
+            item->getRank() == "C" || item->getRank() == "D");
 }
 
 
 // Test case to check if item characteristics are generated correctly
 void RPGSpace_test::testCharacteristicsGeneration() {
-    // Item item;
-    // auto characteristics = item.getCharacteristics();
-    // QVERIFY(!characteristics.empty());
-    // for (const auto& characteristic : characteristics) {
-    //     QVERIFY(characteristic.second >= 1 && characteristic.second <= 8);
-    // }
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    auto characteristics = item->getCharacteristics();
+    QVERIFY(!characteristics.empty());
+    for (const auto& characteristic : characteristics) {
+        QVERIFY(characteristic.second >= 1 && characteristic.second <= 8);
+    }
 }
 
 // Test case to check if item name is generated correctly
 void RPGSpace_test::testNameGeneration() {
-    // Item item;
-    // QVERIFY(!item.getnameOfitem().isEmpty());
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    QVERIFY(!item->getnameOfitem().isEmpty());
 }
 
 // Test case to check if item image is loaded correctly
 void RPGSpace_test::testImageLoading() {
-    // Item item;
-    // QString image = item.getPathToImg();
-    // QVERIFY(!image.isNull());
+    std::shared_ptr<Item> item = ItemFactory::create_by_default();
+    QString image = item->getPathToImg();
+    QVERIFY(!image.isNull());
 }
 
 void RPGSpace_test::testClassDistribution() {
-    // const int sampleSize = 100;
-    // const int expectedCountPerClass = 20;
-    // const int epsilon = 8; // Прийнятий рівень похибки
+    const int sampleSize = 100;
+    const int expectedCountPerClass = 20;
+    const int epsilon = 8; // Прийнятий рівень похибки
 
-    // std::vector<Item> sample;
-    // // Заповнення вибірки з 100 елементів
-    // for (int i = 0; i < sampleSize; ++i) {
-    //     sample.push_back(Item());
-    // }
+    std::vector<std::shared_ptr<Item>> sample;
+    for (int i = 0; i < sampleSize; ++i) {
+        sample.push_back(ItemFactory::create_by_default());
+    }
 
-    // // Підрахунок кількості елементів у кожному класі
-    // int countS = 0, countA = 0, countB = 0, countC = 0, countD = 0;
-    // for (const auto& item : sample) {
-    //     std::string rank = item.getRank().toStdString();
-    //     if (rank == "S") ++countS;
-    //     else if (rank == "A") ++countA;
-    //     else if (rank == "B") ++countB;
-    //     else if (rank == "C") ++countC;
-    //     else if (rank == "D") ++countD;
-    // }
+    int countS = 0, countA = 0, countB = 0, countC = 0, countD = 0;
+    for (const auto& item : sample) {
+        QString rank = item->getRank();
+        if (rank == "S") ++countS;
+        else if (rank == "A") ++countA;
+        else if (rank == "B") ++countB;
+        else if (rank == "C") ++countC;
+        else if (rank == "D") ++countD;
+    }
 
-    // // Перевірка з епсілоном
-    // QVERIFY(qAbs(expectedCountPerClass - countD) <= epsilon);
-    // QVERIFY(qAbs(expectedCountPerClass - countC) <= epsilon);
-    // QVERIFY(qAbs(expectedCountPerClass - countB) <= epsilon);
-    // QVERIFY(qAbs(expectedCountPerClass - countA) <= epsilon);
-    // QVERIFY(qAbs(expectedCountPerClass - countS) <= epsilon);
+    QVERIFY(qAbs(expectedCountPerClass - countD) <= epsilon);
+    QVERIFY(qAbs(expectedCountPerClass - countC) <= epsilon);
+    QVERIFY(qAbs(expectedCountPerClass - countB) <= epsilon);
+    QVERIFY(qAbs(expectedCountPerClass - countA) <= epsilon);
+    QVERIFY(qAbs(expectedCountPerClass - countS) <= epsilon);
 }
 
 
